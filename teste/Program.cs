6bd113445d6f37d9ea26977d409975da6e5966b8
Michael de Almeida <michaeldealmeida@live.com>
2018-03-28 09:13:36 -0300

    Teste do primeiro commit do projeto teste

---
+++ teste/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá mundo");
+            Console.WriteLine("bom dia");
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
